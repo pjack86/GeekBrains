8904bff16fd422eb6d1fbbfa256771df845e5d13
gusev <pjack86@mail/com>
2022-04-27 02:31:25 +0500

    ex01_02

--- Examples/Ex001/Program.cs
+++ Examples/Ex001/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Заказчик уточнил что если числа равно то должно выводиться "Большего числа нет"
+
+int a = 5;
+int b = 7;
+
+if (a < b)
+{
+    Console.WriteLine (b);
+}
+
+if (a > b)
+{
+    Console.WriteLine (a);
+}
+
+if (a == b)
+{
+    Console.WriteLine ("Большего числа нет");
+}
---
+++ Examples/Ex002/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 8;
+int b = 6;
+int c = 9;
+
+int max = a;
+
+if (b > max) 
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine(max);
