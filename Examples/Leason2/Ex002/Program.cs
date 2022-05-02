dea6cc0df1de562596354b4b4a9ef9852af3bc98
gusev <pjack86@mail/com>
2022-05-03 02:08:16 +0500

    L2 Ex002

---
+++ Examples/Leason2/Ex002/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetNumber()
+{
+    int num = 766;
+
+    int a = num / 100;
+
+    int b = (a * 100);
+    int result = (num - b) / 10;
+    return (result);
+}
+Console.WriteLine(GetNumber());
+
+
+
