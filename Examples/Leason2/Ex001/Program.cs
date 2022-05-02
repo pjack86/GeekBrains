db363ae41208c74b6cc6df9e7d7f01eb1a49485e
gusev <pjack86@mail/com>
2022-05-03 01:45:58 +0500

    L2 ex00

---
+++ Examples/Leason2/Ex001/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int GetNumber()
+{
+    int num = new Random().Next(10, 100);
+
+    int a = num / 10;
+    int b = num % 10;
+    Console.WriteLine($" num: {num} a: {a} b: {b}");
+    int max = a;
+
+    if (b > max) max = b;
+    return (max);
+}
+Console.WriteLine(GetNumber());
