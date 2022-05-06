9c98b142794d927d9b6f297e1675dee0e86a541f
gusev <pjack86@mail/com>
2022-05-07 00:07:26 +0500

    L2 ex 004

---
+++ Examples/Leason2/Ex004/Program.cs
@@ -0,0 +1,21 @@
+﻿string Mod(int a)
+{
+    int b = 7;
+    int c = 23;
+    if ((a % b) > 0)
+    {
+        return ("Нет");
+    }
+    else if ((a % c) > 0)
+    {
+        return ("Нет");
+    }
+    else
+    {
+        return ("ДА");
+    }
+}
+
+int a = 161;
+string result = Mod(a);
+Console.WriteLine(result);
