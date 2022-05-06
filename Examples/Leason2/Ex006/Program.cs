3d7f7b8a17f3c8af332150e6cf129114696ada4d
gusev <pjack86@mail/com>
2022-05-07 01:02:42 +0500

    L2 ex 006

---
+++ Examples/Leason2/Ex006/Program.cs
@@ -0,0 +1,15 @@
+﻿int mod()
+{
+    int num = -4356;
+
+    int a = num * -1;
+    int b = num / 1000;
+    int c = (b * 1000);
+    int d = (a - c);
+    int e = (d / 100);
+    int f = (e * 100);
+    int g = (d - f);
+    int result = (g / 10);
+    return (result);
+}
+Console.WriteLine(mod());
