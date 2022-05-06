9e69c0158ea60d66e42d8ff35320c669b82698b6
gusev <pjack86@mail/com>
2022-05-07 00:47:52 +0500

    L2 ex 005

---
+++ Examples/Leason2/Ex005/Program.cs
@@ -0,0 +1,21 @@
+﻿
+string Mod(int a, int b)
+{
+    if (a * a == b)
+    {
+        return ("Да");
+    }
+    else if (b * b == a)
+    {
+        return ("Да");
+    }
+    else
+    {
+        return ("Нет");
+    }
+}
+
+int a = 25;
+int b = 5;
+string result = Mod(a,b);
+Console.WriteLine(result);
