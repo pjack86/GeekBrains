ff755a0c064bc455c7d66f2f0b734d148a61d09d
gusev <pjack86@mail/com>
2022-05-09 18:34:33 +0500

    L3 ex003

---
+++ Examples/Leason3/Ex003/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+//double x1 = 3;
+//double y1 = 6;
+//double z1 = 8;
+//double x2 = 2;
+//double y2 = 1;
+//double z2 = -7;
+
+double Method(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double a = ((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+    double c = Math.Sqrt(a);
+    return (c);
+}
+double c = Method(3, 6, 8, 2, 1, -7);
+Console.WriteLine(c);
