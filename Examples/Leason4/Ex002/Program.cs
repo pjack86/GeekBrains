5e46392268c5cbb084bc9cf037aaa946237eaef3
gusev <pjack86@mail/com>
2022-05-13 17:41:11 +0500

    L4 ex002

---
+++ Examples/Leason4/Ex002/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+long sum(long number)
+{
+    long result = 0;
+
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+long result = sum(11);
+Console.WriteLine(result);
+
