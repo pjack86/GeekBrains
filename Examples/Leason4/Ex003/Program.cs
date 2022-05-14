6edaefc2480649c27c38cddbe5be8a9a652bbf42
gusev <pjack86@mail/com>
2022-05-14 18:20:22 +0500

    a

--- Examples/Leason4/Ex003/Program.cs
+++ Examples/Leason4/Ex003/Program.cs
@@ -1,29 +1,14 @@
 ﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-//void Main()
-//{
-       //     int[] array = new int[8];
-      //      Random myRandom = new Random();
-//            new Random.Next (10000000, 100000000);
- 
-    //        for (int i = 0; i < array.Length; i++)
-  //          {
-   //             array[i] = new Random.Next(0, 8);
-      //          Console.Write("{0} ", array[i]);
-    //        }
-  //      }
-//        Main();
-        //Random myRandom = new Random()
+void arr()
+    {
+        int[] array = new int[8];
+        Random rnd = new Random();
 
-//void arr()
-//    {
-//        int[] array = new int[8];
-//        Random rnd = new Random();
-//
-//        for (int i = 0; i < array.Length; i++)
-//        {
-//            array[i] = rnd.Next(100, 1000);
-//            Console.Write("{0} ", array[i]);
- //       }
-//    }
-//    arr();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(0, 10);
+            Console.Write("{0} ", array[i]);
+       }
+    }
+    arr();
