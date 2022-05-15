cc4f07e79c19bde2fd2257024a33be5fa0a6b0ea
gusev <pjack86@mail/com>
2022-05-15 14:00:07 +0500

    L5 ex001

--- Examples/Leason4/Ex003/Program.cs
+++ Examples/Leason4/Ex003/Program.cs
@@ -1,14 +1,15 @@
 ﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
 void arr()
-    {
-        int[] array = new int[8];
-        Random rnd = new Random();
+{
+    int[] array = new int[8];
+    Random rnd = new Random();
 
-        for (int i = 0; i < array.Length; i++)
-        {
-            array[i] = rnd.Next(0, 10);
-            Console.Write("{0} ", array[i]);
-       }
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 10);
+        Console.Write("{0} ", array[i]);
     }
-    arr();
+}
+arr();
+
---
+++ Examples/Leason5/Ex001/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] CreateArray(int count)
+{
+    int[] result = new int[count];
+    return result;
+}
+
+void FillArray(int[] array)
+{
+    int len = array.Length;
+
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+
+    }
+}
+
+
+int Sum(int[] array)
+{
+    int len = array.Length;
+    int result = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            result = result + 1;
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+int[] arr = CreateArray(7);
+FillArray(arr);
+PrintArray(arr);
+int result = Sum(arr);
+Console.WriteLine(result);
+
